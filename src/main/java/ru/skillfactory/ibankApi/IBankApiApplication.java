952b14cea0ad0e3a9913f19664eb184af9070b56
Kirill Priyatnov <kirill.priyatnov@gmail.com>
2022-10-21 21:48:20 +0300

    first

---
+++ src/main/java/ru/skillfactory/ibankApi/IBankApiApplication.java
@@ -0,0 +1,13 @@
+package ru.skillfactory.ibankApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skillfactory/ibankApi/IBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillfactory.ibankApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
