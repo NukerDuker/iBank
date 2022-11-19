706c28d65c644847908c109a88eeb9c1fb3beb86
k.priyatnov <k.priyatnov@tinkoff.ru>
2022-11-19 18:35:23 +0300

    add docker run cmd in readme

--- src/test/java/ru/skillfactory/ibankApi/IBankApiApplicationTests.java
+++ src/test/java/ru/skillfactory/ibankApi/IBankApiApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class IBankApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
--- src/test/java/ru/skillfactory/ibankApi/service/UserServiceTest.java
+++ src/test/java/ru/skillfactory/ibankApi/service/UserServiceTest.java
@@ -19,29 +19,31 @@ public class UserServiceTest {
     @Autowired
     private UserService userService;
 
+    private long id;
+
     @BeforeEach
     void saveUser() {
         User user = new User();
-        user.setId(2L);
         user.setFirstName("Test");
         user.setLastName("Testing");
         user.setBalance(1L);
         userService.saveUser(user);
+        id = user.getId();
         log.info("User ID: " + userService.getUserRepository().findAll().get(0).getId());
     }
     @Test
     public void getBalance() {
         log.info("User ID: " + userService.getUserRepository().findAll().get(0).getId());
-        Assertions.assertEquals(1, userService.getUserBalance(2L).intValue());
+        Assertions.assertEquals(1, userService.getUserBalance(id).intValue());
     }
     @Test
     public void moneyOperations() throws ControllerException {
         userService.putMoney(1L, 1L);
-        User user = userService.getUserById(1L);
+        User user = userService.getUserById(id);
         Assertions.assertNotEquals(1L, user.getBalance().intValue());
         Assertions.assertEquals(2, user.getBalance());
 
-        userService.takeMoney(1L, 1L);
+        userService.takeMoney(id, 1L);
         Assertions.assertEquals(1, user.getBalance());
     }
 }
