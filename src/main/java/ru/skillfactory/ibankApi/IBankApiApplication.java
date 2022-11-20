7e8f08ec7b6409fc664e8bce3767eae76b09f378
k.priyatnov <k.priyatnov@tinkoff.ru>
2022-11-20 12:38:07 +0300

    add Dockerfile and DTO

--- src/main/java/ru/skillfactory/ibankApi/IBankApiApplication.java
+++ src/main/java/ru/skillfactory/ibankApi/IBankApiApplication.java
@@ -5,12 +5,23 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+
 @SpringBootApplication
 @EnableTransactionManagement
 public class IBankApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IBankApiApplication.class, args);
+
+		float mb = 1024f * 1024f;
+		MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
+
+		float xmx = memoryBean.getHeapMemoryUsage().getMax() / mb;
+		float xms = memoryBean.getHeapMemoryUsage().getInit() / mb;
+		System.out.println("Initial Memory (xms) : " + xms + "mb");
+		System.out.println("Max Memory (xmx) : " + xmx + "mb");
 	}
 
 }
--- src/main/java/ru/skillfactory/ibankApi/controller/UserController.java
+++ src/main/java/ru/skillfactory/ibankApi/controller/UserController.java
@@ -3,6 +3,8 @@ package ru.skillfactory.ibankApi.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.skillfactory.ibankApi.dto.User2UserDtoConverter;
+import ru.skillfactory.ibankApi.dto.UserDto;
 import ru.skillfactory.ibankApi.jsonResponse.JsonResponse;
 import ru.skillfactory.ibankApi.requestModels.ChangeBalanceRequest;
 import ru.skillfactory.ibankApi.requestModels.GetOperationsRequest;
@@ -25,8 +27,10 @@ public class UserController {
     private final OperationsService operationsService;
 
     @GetMapping("/getBalance/{userId}")
-    public User getBalance(@PathVariable Long userId) {
-        return userService.getUserById(userId);
+    public UserDto getBalance(@PathVariable Long userId) {
+        User user = userService.getUserById(userId);
+        User2UserDtoConverter converter = new User2UserDtoConverter();
+        return converter.convertUserToUserDto(user);
     }
 
     @PostMapping("/deposit")
